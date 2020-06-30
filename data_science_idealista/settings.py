7258382cf2f6da5ab56d521760d6fc62bb1c84ef
lucalenq <lucas.alenquer@gmail.com>
2020-06-30 17:48:11 -0400

    Preparing the django app to be deployable on heroku

--- data_science_idealista/settings.py
+++ data_science_idealista/settings.py
@@ -125,3 +125,7 @@ STATIC_URL = '/static/'
 DATASET_DIRS = (
     os.path.join(BASE_DIR,  'polls','static','polls', 'datasets'),
 )
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
